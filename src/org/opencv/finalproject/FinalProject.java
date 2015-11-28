041d69917a7f212e0724d8a02137b0a588cfcc1f
rmahieu <rmahieu0@gmail.com>
2015-11-27 17:06:45 -0800

    New button performs real-time updates
    Changed "Canny" button to "All Processing" which now bundles all the
    app's functionality. The "All Processing" function does thresholding,
    detects corners, then draws the correspondingly warped image every each
    frame.  Also moved each of these processes to it's own function if it
    didn't already have its own.
    
    Also added error popup functionality. If user hasn't selected an image,
    or hasn't selected a threshold color, the app prompts them to do so.

--- src/org/opencv/finalproject/FinalProject.java
+++ src/org/opencv/finalproject/FinalProject.java
@@ -15,7 +15,6 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.android.CameraBridgeViewBase;
@@ -24,6 +23,8 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.finalproject.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -45,12 +46,14 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
 
     private static final int       VIEW_MODE_RGBA     = 0;
     private static final int       VIEW_MODE_COLOR_THRESHOLD     = 1;
-    private static final int       VIEW_MODE_CANNY    = 2;
+    private static final int       VIEW_MODE_ALL_PROCESSING    = 2;
 
+    private static final int	   ERR_NO_COLOR = 0;
+    private static final int	   ERR_NO_USER_IMAGE = 1;
+    
     private int                    mViewMode;
     private Mat                    mRgba;
     private Mat                    mIntermediateMat;
-//    private Mat					   mHarrisCornerMat;
     
     private Point[]				   mCamCorners = new Point[4];
     
@@ -62,10 +65,11 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
     private Boolean				   mTouchEvent = false;
 
     private int					   mFrameCounter = 0;
+    private int					   mErr;
     
     private MenuItem               mItemPreviewRGBA;
     private MenuItem               mItemPreviewColorThresholded;
-    private MenuItem               mItemPreviewCanny;
+    private MenuItem               mItemPreviewAllProcessing;
     private MenuItem			   mItemChooseImage;
 
     private Uri 				   selectedImageURI = null;
@@ -162,7 +166,7 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
         Log.i(TAG, "called onCreateOptionsMenu");
         mItemPreviewRGBA = menu.add("Preview RGBA");
         mItemPreviewColorThresholded = menu.add("Preview Thresholded");
-        mItemPreviewCanny = menu.add("Canny");
+        mItemPreviewAllProcessing = menu.add("All Processing");
         mItemChooseImage = menu.add("Choose Image");
         return true;
     }
@@ -197,17 +201,14 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
 
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat(height, width, CvType.CV_8UC4);
-        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
-        //mHarrisCornerMat = new Mat(height, width, CvType.CV_32FC1);
-        //mColorData = new Scalar(0,0,0,0);        
+        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);      
     }
 
     public void onCameraViewStopped() {
         mRgba.release();
         mIntermediateMat.release();
-        //mHarrisCornerMat.release();
     }
-
+    
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {  	
     	final int viewMode = mViewMode;
         mRgba = inputFrame.rgba();
@@ -225,128 +226,49 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
         	
         	// Perform color thresholding (right now the threshold is hard-coded)        	
         	double thresh = 1.5;
-        	
-        	Scalar uThresh = new Scalar(thresh,thresh,thresh,0);
-        	Scalar lThresh = new Scalar(1/thresh,1/thresh,1/thresh,0);
-        	
-        	Scalar uBound = mColorData.mul(uThresh);
-        	Scalar lBound = mColorData.mul(lThresh);
-        	
-        	lBound.val[3] = 0;
-        	uBound.val[3] = 255;
-        	
-        	Core.inRange(mRgba, lBound, uBound, mIntermediateMat);
-//        	Imgproc.cvtColor(mIntermediateMat, mIntermediateMat, Imgproc.COLOR_GRAY2RGBA);
+        	colorThreshold(mRgba, mIntermediateMat, thresh);
         	        	
         	// Low Pass Filter
         	Imgproc.GaussianBlur(mIntermediateMat, mIntermediateMat, new Size(5,5), 10.0);        	
         	
         	// Harris Corners
-        	getHarrisCorners();
-        	        	        	
-        	// Draw information regarding thresholding
-        	boolean drawThreshInfo = false;
-        	if(drawThreshInfo) {
-	        	Core.putText(mIntermediateMat, "Color = " + mColorData.toString(), new Point(50,mIntermediateMat.rows()-150),
-						Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255,0,125,255), 2);
-	        	Core.putText(mIntermediateMat, "LB = " + lBound.toString(), new Point(50,mIntermediateMat.rows()-100),
-						Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255,0,125,255), 2);
-	        	Core.putText(mIntermediateMat, "UB = " + uBound.toString(), new Point(50,mIntermediateMat.rows()-50),
-						Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255,0,125,255), 2);
-        	}
-        	        	
+        	getHarrisCorners(true);
+        	        	        	        	        	
         	mRgba = mIntermediateMat;
             break;
             
         case VIEW_MODE_RGBA:
-            // input frame has RBGA format
-        	
-            if(mBitmap != null) {
-            	// Convert user image to Mat
-            	mUserImg = new Mat();
-            	Utils.bitmapToMat(mBitmap, mUserImg);
-            	
-            	// Determine organization of detected corners
-            	// First separate left and right corners
-            	int[] leftPts = new int[2];
-            	int[] rightPts = new int[2];
-            	int lCount = 0;
-    			int rCount = 0;
-
-				// Find median of x coordinates
-				double[] xCoords = {mCamCorners[0].x, mCamCorners[1].x, mCamCorners[2].x, mCamCorners[3].x};
-    			Arrays.sort(xCoords);
-    			double xMed = (xCoords[xCoords.length/2] + xCoords[xCoords.length/2-1])/2;
-    			
-    			// Compare each x coordinate to the median
-            	for(int i=0; i<4; i++) {
-            		if(mCamCorners[i].x <= xMed) {
-            			leftPts[lCount] = i;
-            			lCount += 1;
-            		}
-            		else {
-            			rightPts[rCount] = i;
-            			rCount += 1;
-            		}
-            	}
-            	
-            	// Then separate top and bottom corners
-            	int tl_idx, bl_idx, tr_idx, br_idx;
-            	if(mCamCorners[leftPts[0]].y > mCamCorners[leftPts[1]].y) {
-            		tl_idx = leftPts[1];
-            		bl_idx = leftPts[0];
-            	}
-            	else {
-            		tl_idx = leftPts[0];
-            		bl_idx = leftPts[1];
-            	}
-            	if(mCamCorners[rightPts[0]].y > mCamCorners[rightPts[1]].y) {
-            		tr_idx = rightPts[1];
-            		br_idx = rightPts[0];
-            	}
-            	else {
-            		tr_idx = rightPts[0];
-            		br_idx = rightPts[1];
-            	}
-            	
-            	// Reorganize mCamCorners
-            	Point[] orgCorners = {mCamCorners[tl_idx], mCamCorners[tr_idx], mCamCorners[br_idx], mCamCorners[bl_idx]};
-            	
-            	// Create homography matrix from user img --> camera rectangle
-            	MatOfPoint2f dest = new MatOfPoint2f(orgCorners);
-            	Point[] imgCorners = {
-        					new Point(0,0),
-        					new Point(mUserImg.rows(),0),
-            				new Point(mUserImg.rows(),mUserImg.cols()),
-							new Point(0,mUserImg.cols()) };
-            	MatOfPoint2f source = new MatOfPoint2f(imgCorners);
-            	
-            	Mat H = Calib3d.findHomography(source, dest);
-
-            	// Warp user img to camera rect
-            	Mat warpImg = new Mat();
-            	Imgproc.warpPerspective(mUserImg, warpImg, H, mRgba.size());
-            	
-            	// Generate corresponging mask
-            	Mat imgMask = new Mat();
-            	Imgproc.threshold(warpImg, imgMask, 0.1, 255, Imgproc.THRESH_BINARY);
-            	
-            	// Copy warped user image to correct location in camera img
-            	warpImg.copyTo(mRgba, imgMask);
-            }
-        	
+            // input frame has RBGA format        	
             break;
             
-        case VIEW_MODE_CANNY:
-            // input frame has gray scale format
-            Imgproc.Canny(inputFrame.gray(), mIntermediateMat, 80, 100);
-            Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+        case VIEW_MODE_ALL_PROCESSING:        	
+        	// Make sure thresh color AND user image have been selected
+        	if (mColorData == null) {
+        		displayPopup(ERR_NO_COLOR);
+        		break;
+        	}
+        	if (mUserImg == null) {
+        		displayPopup(ERR_NO_USER_IMAGE);
+        		break;
+        	}
+        	
+        	// Color threshold
+        	double thresh2 = 1.5;
+        	colorThreshold(mRgba, mIntermediateMat, thresh2);
+        	
+        	// Get corners
+        	Imgproc.GaussianBlur(mIntermediateMat, mIntermediateMat, new Size(5,5), 10.0);        	
+        	getHarrisCorners(false);
+        	
+        	// Draw image
+        	drawWarpedImg(mUserImg);
+        	
             break;
         }
 
     	// Draw color circle where user touched screen 
         if(mTouchEvent) {        	
-        	// Can draw the coordinates to screen for debugging purposes
+//        	// Can draw the coordinates to screen for debugging purposes
 //        	Core.putText(mRgba, mTouchPoint.y + " x " + mTouchPoint.x, new Point(20,mRgba.rows()-20),
 //					Core.FONT_HERSHEY_SIMPLEX, 0.3, new Scalar(255,60,60,255), 2);
         	
@@ -364,8 +286,8 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
             mViewMode = VIEW_MODE_RGBA;
         } else if (item == mItemPreviewColorThresholded) {
             mViewMode = VIEW_MODE_COLOR_THRESHOLD;
-        } else if (item == mItemPreviewCanny) {
-            mViewMode = VIEW_MODE_CANNY;
+        } else if (item == mItemPreviewAllProcessing) {
+            mViewMode = VIEW_MODE_ALL_PROCESSING;
         } else if (item == mItemChooseImage) {
             Intent intent = new Intent();
             intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -378,30 +300,35 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
         return true;
     }
     
-    private void getHarrisCorners()
+    private void colorThreshold(Mat inputImg, Mat outputImg, double thresh)
+    {
+    	// Define 4D threshold scalars
+    	Scalar uThresh = new Scalar(thresh,thresh,thresh,0);
+    	Scalar lThresh = new Scalar(1/thresh,1/thresh,1/thresh,0);
+    	
+    	Scalar uBound = mColorData.mul(uThresh);
+    	Scalar lBound = mColorData.mul(lThresh);
+    	
+    	// Make sure opacity thresholds are set to include all
+    	lBound.val[3] = 0;
+    	uBound.val[3] = 255;
+    	
+    	// Do color thresholding on image
+    	Core.inRange(inputImg, lBound, uBound, outputImg);
+    }
+    
+    private void getHarrisCorners(boolean drawCircles)
     {    	
-    	/// Detector parameters
+    	// Detector parameters
     	int blockSize = 7;
     	int apertureSize = 5;
     	double k = 0.015;
-//    	double thresh = 0.3;
 
-    	/// Detect corners
+    	// Detect corners
     	Mat harrisCornerMat = new Mat();
     	Imgproc.cornerHarris(mIntermediateMat, harrisCornerMat, blockSize, apertureSize, k, Imgproc.BORDER_DEFAULT);
-    
-    	/// Draw a circle around corners (for illustration purposes, comment out for performance)
-//		Imgproc.cvtColor(mIntermediateMat, mIntermediateMat, Imgproc.COLOR_GRAY2RGBA, 4);
-//    	for( int j = 0; j < mHarrisCornerMat.rows() ; j++ ) {
-//    		for( int i = 0; i < mHarrisCornerMat.cols(); i++ ) {
-//    			if(mHarrisCornerMat.get(j,i)[0] > thresh)
-//    	        {
-//    				Core.circle(mIntermediateMat, new Point(i,j), 5, new Scalar(0,255,0,255), 1);
-//    	        }
-//    	    }
-//    	}
-    	
-    	/// Get most prominent 4 corners
+        	
+    	// Get most prominent 4 corners
     	for(int i = 0; i < 4; i++) {
     		// Find location of largest peak
     		Core.MinMaxLocResult searchResult = Core.minMaxLoc(harrisCornerMat);
@@ -411,13 +338,84 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
     		Core.circle(harrisCornerMat, searchResult.maxLoc, 5, new Scalar(0), -1);
     	}
     	
-    	/// Draw circles at corners
-    	Imgproc.cvtColor(mIntermediateMat, mIntermediateMat, Imgproc.COLOR_GRAY2RGBA, 4);
-    	for(int i = 0; i < 4; i++) {
-    		Core.circle(mIntermediateMat, mCamCorners[i], 5, new Scalar(0,255,0,255), 1);
+    	if(drawCircles) {
+	    	// Draw circles at corners
+	    	Imgproc.cvtColor(mIntermediateMat, mIntermediateMat, Imgproc.COLOR_GRAY2RGBA, 4);
+	    	for(int i = 0; i < 4; i++) {
+	    		Core.circle(mIntermediateMat, mCamCorners[i], 5, new Scalar(0,255,0,255), 1);
+	    	}
+    	}
+    }
+    
+    private void drawWarpedImg(Mat inputImg)
+    {
+    	// Determine organization of detected corners
+    	// First separate left and right corners
+    	int[] leftPts = new int[2];
+    	int[] rightPts = new int[2];
+    	int lCount = 0;
+		int rCount = 0;
+
+		// Find median of x coordinates
+		double[] xCoords = {mCamCorners[0].x, mCamCorners[1].x, mCamCorners[2].x, mCamCorners[3].x};
+		Arrays.sort(xCoords);
+		double xMed = (xCoords[xCoords.length/2] + xCoords[xCoords.length/2-1])/2;
+		
+		// Compare each x coordinate to the median
+    	for(int i=0; i<4; i++) {
+    		if(mCamCorners[i].x <= xMed) {
+    			leftPts[lCount] = i;
+    			lCount += 1;
+    		}
+    		else {
+    			rightPts[rCount] = i;
+    			rCount += 1;
+    		}
     	}
     	
+    	// Then separate top and bottom corners
+    	int tl_idx, bl_idx, tr_idx, br_idx;
+    	if(mCamCorners[leftPts[0]].y > mCamCorners[leftPts[1]].y) {
+    		tl_idx = leftPts[1];
+    		bl_idx = leftPts[0];
+    	}
+    	else {
+    		tl_idx = leftPts[0];
+    		bl_idx = leftPts[1];
+    	}
+    	if(mCamCorners[rightPts[0]].y > mCamCorners[rightPts[1]].y) {
+    		tr_idx = rightPts[1];
+    		br_idx = rightPts[0];
+    	}
+    	else {
+    		tr_idx = rightPts[0];
+    		br_idx = rightPts[1];
+    	}
     	
+    	// Reorganize mCamCorners
+    	Point[] orgCorners = {mCamCorners[tl_idx], mCamCorners[tr_idx], mCamCorners[br_idx], mCamCorners[bl_idx]};
+    	
+    	// Create homography matrix from user img --> camera rectangle
+    	MatOfPoint2f dest = new MatOfPoint2f(orgCorners);
+    	Point[] imgCorners = {
+					new Point(0,0),
+					new Point(inputImg.rows(),0),
+    				new Point(inputImg.rows(),inputImg.cols()),
+					new Point(0,inputImg.cols()) };
+    	MatOfPoint2f source = new MatOfPoint2f(imgCorners);
+    	
+    	Mat H = Calib3d.findHomography(source, dest);
+
+    	// Warp user img to camera roi
+    	Mat warpImg = new Mat();
+    	Imgproc.warpPerspective(inputImg, warpImg, H, mRgba.size());
+    	
+    	// Generate corresponding mask
+    	Mat imgMask = new Mat();
+    	Imgproc.threshold(warpImg, imgMask, 0.1, 255, Imgproc.THRESH_BINARY);
+    	
+    	// Copy warped user image to correct location in camera img
+    	warpImg.copyTo(mRgba, imgMask);
     }
     
     // Functions to select Bitmap from user's photos
@@ -434,6 +432,10 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
                 } catch (IOException e) {};
                 
                 mBitmap = scaled_bmp;
+                
+                // Convert Bitmap to Mat
+                mUserImg = new Mat();
+            	Utils.bitmapToMat(mBitmap, mUserImg);
             }                
         }
     }
@@ -446,5 +448,57 @@ public class FinalProject extends Activity implements CvCameraViewListener2 {
         parcelFileDescriptor.close();
         return image;
     }
-
+    
+    // Display error popup
+    private void displayPopup(final int err)
+    {
+    	mViewMode = VIEW_MODE_RGBA;
+    	mErr = err;
+    	
+        FinalProject.this.runOnUiThread(new Runnable() {
+        	public void run() {
+		    	AlertDialog.Builder alertBuilder = new AlertDialog.Builder(FinalProject.this);
+		    	alertBuilder.setTitle("Error:");
+		    	
+		    	// Message dependent on specific error
+		    	switch(mErr) {
+		    	case(ERR_NO_COLOR):
+		    		alertBuilder.setMessage("Please select a threshold color...");
+		    		break;
+		    	case(ERR_NO_USER_IMAGE):
+		    		alertBuilder.setMessage("Please select an image...");
+	    			break;
+		    	}		  
+		    	
+		    	alertBuilder.setPositiveButton("OK",
+		            new DialogInterface.OnClickListener() {
+		            	public void onClick(DialogInterface dialog, int id) {
+		    		    	
+		            		// Result dependent on specific error
+		            		switch(mErr) {
+		    		    	case(ERR_NO_COLOR):
+		    		    		mViewMode = VIEW_MODE_RGBA;
+		    		    		break;
+		    		    	case(ERR_NO_USER_IMAGE):
+		    		    		onOptionsItemSelected(mItemChooseImage);
+		    		    		mViewMode = VIEW_MODE_ALL_PROCESSING;
+		    	    			break;
+		    		    	}	
+		            		
+		            		dialog.dismiss();
+		            	}
+		        	});
+		    	alertBuilder.setNegativeButton("Cancel", 
+					new DialogInterface.OnClickListener() {
+		            	public void onClick(DialogInterface dialog, int id) {
+		            		mViewMode = VIEW_MODE_RGBA;
+		            		dialog.dismiss();
+		            	}
+		        	});
+		        AlertDialog alertBox = alertBuilder.create();
+        		alertBox.show();
+        	}
+        });
+    }
+    
 }
